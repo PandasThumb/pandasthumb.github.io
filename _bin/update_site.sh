f820279ce6d305106038eb56f8a27848126ee2a8
Reed A. Cartwright <cartwright@asu.edu>
2016-11-26 21:43:24 -0700

    Improve Webhook

--- _bin/update_site.sh
+++ _bin/update_site.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
+# redirect stderr to stdout
+exec 2>&1
+
+PROGNAME=`basename $0`
+
+error_exit()
+{
+	echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+	exit 1
+}
 
 src="$1/source/"
 dest="$1/public_html/"
 key="$1/key/id_rsa"
 
-cd "$src" || exit 1
-echo "Updating Webpage Source..."
+cd "$src" || error_exit "ERROR: source directory does not exist."
+
+echo "[====] Updating Webpage Source [====]"
 GIT_SSH_COMMAND="ssh -i '$key' -o StrictHostKeyChecking=no"
 export GIT_SSH_COMMAND
-git checkout master && git pull origin || exit 1
-
-echo "Building Webpage..."
-jekyll build --incremental || exit 1
+git checkout master && git pull origin master || error_exit "ERROR: Updating webpage via Git failed."
 
 echo
-echo "Installing Webpage..."
-rsync -av --delete --chown www:www _site/ "$dest"
+echo "[====] Building Webpage [====]"
+jekyll build --incremental || error_exit "ERROR: Building webpage via Jekyll failed."
+
+echo
+echo "[====] Installing Webpage [====]"
+rsync -avc --delete --chown www:www _site/ "$dest" || error_exit "ERROR: Installing new webpage via rsync failed."
--- _bin/webhook.rb
+++ _bin/webhook.rb
@@ -17,14 +17,19 @@ post '/updatewebsite' do
   repo = push["repository"]["full_name"]
   dir = sites[repo]
   if(dir) then
-  	output = `sh /usr/local/www/scripts/update_site.sh "#{dir}"`
-  	puts output
+    email = 'www'
+    puts "Triggering rebuild of website #{repo} and emailing results to #{email}."
+    pid = spawn("sh /usr/local/www/scripts/update_site.sh '#{dir}' | mail -s 'Rebuild of #{repo}' '#{email}'")
+    Process.detach pid
   else
   	return halt 500, "Repository #{repo} not recognized."
   end
 end
 
 def verify_signature(payload_body)
+  if ENV['WEBHOOK_SECRET'].nil?
+    return halt 500, "Server-side secret not properly configured!"
+  end
   signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['WEBHOOK_SECRET'], payload_body)
   return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
 end
