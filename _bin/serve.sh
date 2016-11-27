44bd5f6f6ec581f5466fce5b26abf7f15ec47bb9
Reed A. Cartwright <cartwright@asu.edu>
2016-11-27 00:13:03 -0700

    Limit number of posts build by a local server: faster times Only display disqus on production builds. Simplify figure styles by specify figure widths.

--- _bin/serve.sh
+++ _bin/serve.sh
@@ -5,4 +5,4 @@ if [ -f _config_local.yml ]; then
 	config=${config},_config_local.yml
 fi
 
-jekyll serve --config $config --watch --incremental
+jekyll serve --config $config --watch --incremental --limit_posts 5
