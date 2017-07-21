697349464a341caea34dc9c513d83efdb228066a
Reed A. Cartwright <cartwright@asu.edu>
2017-07-21 15:38:18 -0700

    Modify how plugins are loaded

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'jekyll', '3.5.1'
+
+group :jekyll_plugins do
+  gem 'jekyll-feed'
+  gem 'jekyll-archives', :git => 'https://github.com/jekyll/jekyll-archives', :branch => 'master'
+end
--- _bin/serve.sh
+++ _bin/serve.sh
@@ -5,6 +5,6 @@ if [ -f _config_local.yml ]; then
 	config=${config},_config_local.yml
 fi
 
-jekyll serve --config $config --watch --incremental
+bundle exec jekyll serve --config $config --watch --incremental
 
 #--limit_posts 5
